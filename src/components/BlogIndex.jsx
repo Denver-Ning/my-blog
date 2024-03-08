7aa5c06769cdf02d1c363edca8082cac099798a4
zhangshuaining <1208505019@qq.com>
2024-03-08 15:53:17 +0800

    fix: jsx className

--- src/components/BlogIndex.jsx
+++ src/components/BlogIndex.jsx
@@ -2,10 +2,10 @@ import './BlogIndex.less'
 
 const BlogIndex = ({date,title,desc,url}) => {
   return (
-    <div class="list-container">
-      <div class="date">{date}</div>
-      <a href={url} class="title">{title}</a>
-      <div class="desc">{desc}</div>
+    <div className="list-container">
+      <div className="date">{date}</div>
+      <a href={url} className="title">{title}</a>
+      <div className="desc">{desc}</div>
     </div>
   )
 }
